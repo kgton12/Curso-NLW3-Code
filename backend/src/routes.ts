aa1eb0181dd4994fe6212c712a84ae1dd6b27ec5
Welington Oliveira <56723945+kgton12@users.noreply.github.com>
2020-11-24 10:10:04 -0300

    NLW

---
+++ backend/src/routes.ts
@@ -0,0 +1,16 @@
+import { Router } from 'express';
+import multer from 'multer';
+
+import uploadConfig from './config/upload';
+import OrphangesController from './controllers/OrphanagesController';
+
+
+const routes = Router();
+const upload = multer(uploadConfig);
+
+// index show create update delete
+routes.get('/orphanages', OrphangesController.index);
+routes.get('/orphanages/:id', OrphangesController.show);
+routes.post('/orphanages', upload.array('images'), OrphangesController.create);
+
+export default routes;
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,13 +1,17 @@
-import express from 'express';
-
+import express, { request } from 'express';
+import path from 'path';
+import 'express-async-errors';
 import './database/connection';
+import routes from './routes';
+import errorHandler from './errors/handler';
+
 
 const app = express();
 
 app.use(express.json());
+app.use(routes);
+app.use('/uploads', express.static(path.join(__dirname, '..', 'uploads')));
+app.use(errorHandler);
 
-app.get('/users', (request,response) => {
-  return response.json({message:"Hello Word"})
-});
 
 app.listen(3333);
