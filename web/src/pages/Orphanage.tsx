ce077704eedc082548a9b09757049681aec4e494
Welington Oliveira <56723945+kgton12@users.noreply.github.com>
2020-12-14 20:20:02 -0300

    NLW

--- web/src/pages/Orphanage.tsx
+++ web/src/pages/Orphanage.tsx
@@ -1,12 +1,12 @@
-import React, {useEffect, useState} from "react";
-import {FaWhatsapp} from "react-icons/fa";
-import {FiClock, FiInfo} from "react-icons/fi";
-import {MapContainer, Marker, TileLayer} from "react-leaflet";
+import React, { useEffect, useState } from "react";
+import { FaWhatsapp } from "react-icons/fa";
+import { FiClock, FiInfo } from "react-icons/fi";
+import { MapContainer, Marker, TileLayer } from "react-leaflet";
 import '../styles/pages/orphanage.css';
 import Sidebar from "../components/Sidebar";
 import mapIcon from "../utils/mapIcon";
 import api from "../services/api";
-import {useParams} from 'react-router-dom';
+import { useParams } from 'react-router-dom';
 
 interface Orphanage {
     id: number,
@@ -34,32 +34,28 @@ export default function Orphanage() {
     useEffect(() => {
         api.get(`orphanages/${params.id}`).then(response => {
             setOrphanage(response.data);
-          //console.log(response);
+            //console.log(response);
         })
     }, [params.id])
 
     if (!orphanage) {
         return <p>Carregando...</p>;
-
     }
-
     return (
         <div id="page-orphanage">
-            <Sidebar/>
+            <Sidebar />
 
             <main>
-                <div className="orphanage-details">
-                    <img src={orphanage.images[0].url} alt={orphanage.nome}/>
-
+                <div className="orphanage-details">                    
+                    <img src={orphanage.images[0].url} alt={orphanage.nome} />
                     <div className="images">
                         {orphanage.images.map(image => {
                             <button key={image.id} className="active" type="button">
+                                              
                                 <img src={image.url}
-                                     alt={orphanage.nome}/>
+                                    alt={orphanage.nome} />
                             </button>
                         })}
-
-
                     </div>
 
                     <div className="orphanage-details-content">
@@ -72,7 +68,7 @@ export default function Orphanage() {
                             <MapContainer
                                 center={[orphanage.latitude, orphanage.longitude]}
                                 zoom={16}
-                                style={{width: '100%', height: 280}}
+                                style={{ width: '100%', height: 280 }}
                                 dragging={false}
                                 touchZoom={false}
                                 zoomControl={false}
@@ -83,7 +79,7 @@ export default function Orphanage() {
                                     url={`https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_TOKEN}`}
                                 />
                                 <Marker interactive={false} icon={mapIcon}
-                                        position={[orphanage.latitude, orphanage.longitude]}/>
+                                    position={[orphanage.latitude, orphanage.longitude]} />
                             </MapContainer>
 
                             <footer>
@@ -91,30 +87,30 @@ export default function Orphanage() {
                             </footer>
                         </div>
 
-                        <hr/>
+                        <hr />
 
                         <h2>Instruções para visita</h2>
                         <p>{orphanage.instructions}</p>
 
                         <div className="open-details">
                             <div className="hour">
-                                <FiClock size={32} color="#15B6D6"/>
-                                Segunda à Sexta <br/>
+                                <FiClock size={32} color="#15B6D6" />
+                                Segunda à Sexta <br />
                                 {orphanage.opening_hours}
                             </div>
                             {orphanage.open_on_weekends ? (
                                 <div className="open-on-weekends">
-                                    <FiInfo size={32} color="#39CC83"/>
-                                    Atendemos <br/>
+                                    <FiInfo size={32} color="#39CC83" />
+                                    Atendemos <br />
                                     fim de semana
                                 </div>
                             ) : (
-                                <div className="open-on-weekends dont-open">
-                                    <FiInfo size={32} color="#FF669D"/>
-                                    Não Atendemos <br/>
+                                    <div className="open-on-weekends dont-open">
+                                        <FiInfo size={32} color="#FF669D" />
+                                    Não Atendemos <br />
                                     fim de semana
-                                </div>
-                            )}
+                                    </div>
+                                )}
                         </div>
 
                         {/*<button type="button" className="contact-button">
