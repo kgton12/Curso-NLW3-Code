05016da737f7eb7acfb92bfe66fb7bf14ac5576d
Welington Oliveira <56723945+kgton12@users.noreply.github.com>
2020-12-23 16:18:03 -0300

    NLW

--- mobile/src/routes.tsx
+++ mobile/src/routes.tsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 
@@ -5,14 +6,39 @@ const { Navigator, Screen } = createStackNavigator();
 
 import OrphanagesMap from './pages/OrphanagesMap';
 import OrphanagesDetails from './pages/OrphanageDetails';
+
+import SelectMapPosition from './pages/CreateOrphanage/SelectMapPosition';
+import OrphanageData from './pages/CreateOrphanage/OrphanageData';
+
+import Header from './components/Header';
 //headerShown: false  = retira o header de todas as paginas.
 export default function Routes() {
-    return (
-        <NavigationContainer>
-            <Navigator screenOptions={{ headerShown: false }}>
-                <Screen name="OrphanagesMap" component={OrphanagesMap} />
-                <Screen name="OrphanagesDetails" component={OrphanagesDetails} />
-            </Navigator>
-        </NavigationContainer>
-    );
+	return (
+		<NavigationContainer>
+			<Navigator screenOptions={{ headerShown: false, cardStyle: { backgroundColor: '#f2f3f5' } }}>
+				<Screen name="OrphanagesMap" component={OrphanagesMap} />
+				<Screen name="OrphanagesDetails"
+					component={OrphanagesDetails}
+					options={{
+						headerShown: true,
+						header: () => <Header showCancel={false} title="Orfanato" />//Header criado
+					}}
+				/>
+				<Screen name="SelectMapPosition"
+					component={SelectMapPosition}
+					options={{
+						headerShown: true,
+						header: () => <Header title="Seleciona no mapa" />//Header criado
+					}}
+				/>
+				<Screen name="OrphanageData"
+					component={OrphanageData}
+					options={{
+						headerShown: true,
+						header: () => <Header title="Informe os dados" />//Header criado
+					}}
+				/>
+			</Navigator>
+		</NavigationContainer>
+	);
 }
